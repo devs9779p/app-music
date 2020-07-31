925f391fdc9ddad1693c28faf31c4829e0502364
fabia villarruel <fabiaalicia@protonmail.com>
2020-07-31 16:42:35 -0300

    modifique gitignore, agregue sass, nuevo player.html

--- client/src/app/components/player.component.ts
+++ client/src/app/components/player.component.ts
@@ -4,33 +4,7 @@ import { GLOBAL } from '../services/global';
 
 @Component({
     selector: 'player',
-    template: `
-    <div class="album-image">
-        <span *ngIf="song.album">
-            <img id="play-image-album" src="{{url + 'get-image-album/' + song.album.image}}">
-        </span>
-        <span *ngIf="!song.album">
-            <img id="play-image-album" src="assets/images/corchea.png">
-        </span >
-    </div>
-
-    <div class="audio-file">
-        <p id="Nomenclatura">Reproduciendo</p>
-        <span id="play-song-title">
-            {{song.name}}
-        </span>
-        |
-        <span id="play-song-artist">
-            <span *ngIf="song.album.artist">
-                {{song.album.artist.name}}
-            </span>
-        </span>
-        <audio controls id="player">
-            <source id="mp3-source" src="{{url + 'get-file-song/' + song.file}}" type="audio/mpeg">
-            Tu navegador no es compatible para reproducir el contenido
-        </audio>
-    </div>
-    ` 
+    templateUrl: '../views/player.html',
 })
 
 export class PlayerComponent implements OnInit{
